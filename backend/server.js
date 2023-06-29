9b32d585c120fc2a22aa174b6ef6f28040a91fc1
sunnyvk <khambayatsunny278@gmail.com>
2023-06-29 11:13:52 +0530

    first

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+const app=require("./app");
+
+const dotenv=require("dotenv");
+const connectDatabase=require("./config/database")
+const cors=require('cors')
+app.use(cors({
+  origin:"*"
+})
+);
+// Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Uncaught Exception`);
+    process.exit(1);
+  });
+//   console.log(youtube)
+//Config
+
+dotenv.config({path:"backend/config/config.env"});
+
+//Connecting to database
+
+connectDatabase()
+
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server is working on http://localhost:${process.env.PORT}`);
+  });
+  
+  // Unhandled Promise Rejection
+  process.on("unhandledRejection", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+  
+    server.close(() => {
+      process.exit(1);
+    });
+  });
